443972321625a071c4051897e12d3090ff422597
Ben Moss <benm@vmware.com>
2021-06-08 14:19:27 -0400

    more fun

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"os"
 )
+
+type GithubEvent struct {
+	Issue struct {
+		Number int
+	}
+	Repository struct {
+		FullName string
+	}
+}
 
 func main() {
 	bs, err := os.ReadFile(os.Getenv("GITHUB_EVENT_PATH"))
@@ -11,6 +21,11 @@ func main() {
 		log.Fatalf("failed to read github event: %v", err)
 	}
 	log.Println(string(bs))
+	var event GithubEvent
+	if err := json.Unmarshal(bs, &event); err != nil {
+		log.Fatalf("failed to unmarshal event: %v", err)
+	}
+	log.Println(event)
 	// ctx := context.Background()
 	// ts := oauth2.StaticTokenSource{
 	// 	&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
